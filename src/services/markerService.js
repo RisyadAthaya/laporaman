115e058e867ede9eab6f183111f54f735a4010cf
Athaya <risyad.athaya2007@gmail.com>
2026-07-16 20:41:41 +0700

    feat(database): add firestore for markers database

---
+++ src/services/markerService.js
@@ -0,0 +1,34 @@
+import { collection, doc, setDoc, getDocs } from "firebase/firestore";
+import { database } from "./firebaseConfig.js";
+
+export const saveMarker = async (markerData) => {
+  try {
+    const docRef = doc(collection(database, "markers"));
+    const cloudId = docRef.id;
+
+    const finalMarkerData = {
+      ...markerData,
+      key: cloudId
+    }
+
+    await setDoc(docRef, finalMarkerData);
+
+    return docRef.id;
+  } catch (error) {
+    console.error("Error adding marker to backend: ", error);
+  }
+};
+
+export const fetchAllPinpoints = async () => {
+  try {
+    const querySnapshot = await getDocs(collection(database, "markers"));
+
+    // Transform Firebase's raw snapshot into an array
+    return querySnapshot.docs.map(doc => ({
+      ...doc.data()
+    }));
+  } catch (error) {
+    console.error("Error fetching pinpoints from backend: ", error);
+    return [];
+  }
+};
