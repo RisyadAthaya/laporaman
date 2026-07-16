3cf39c340248948d08de7febd0584f92d462a9aa
DioDateUs <deodatussihaloho@gmail.com>
2026-07-17 02:47:38 +0700

    add Dashboard.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 import SettingsPage from "./pages/SettingsPage.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')).render(
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
