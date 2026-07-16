82a4e7a4386003c24224a2f2e4cf45d86881b443
DioDateUs <deodatussihaloho@gmail.com>
2026-07-17 02:35:22 +0700

    tes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
+import BrandLogo from "./BrandLogo.jsx";
+
 function Footer() {
   return (
-      <footer className="landing-page-footer flex justify-end h-15 border-t-[3px] border-t-border">
-        <p className="text-dark font-bold ml-auto">Temukan jalan yang aman</p>
+      <footer className="header1">
+        <div className="header-title w-full  px-8 flex justify-between">
+          <BrandLogo
+              className="flex items-center gap-[6.438px]"
+              imgStyle={{ width: "32px", height: "38.5px" }}
+          />
+          <span className="text-[12px] md:text-sm text-text3/90 font-medium font-sans">
+          © 2026 Orang Desa yang Pake Dolar GarudaHacks 7.0
+        </span>
+        </div>
       </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
