d40cea9cbf59764b9a77f406ccdaeeb628399a73
Athaya <risyad.athaya2007@gmail.com>
2026-07-16 17:13:02 +0700

    feat(map): add simple map interactivity

--- src/components/MapInteractive.jsx
+++ src/components/MapInteractive.jsx
@@ -1,7 +1,21 @@
-import Map from 'react-map-gl/maplibre'
-import 'maplibre-gl/dist/maplibre-gl.css';
+import { Map, Marker, Popup } from 'react-map-gl/maplibre'
+import 'maplibre-gl/dist/maplibre-gl.css'
+import { useState } from 'react'
 
 function MapInteractive() {
+  const markersData = [
+    {
+      key: 1,
+      latitude: -6.175,
+      longitude: 106.825,
+      title: "Rumah Ihsan",
+      description: "Ihsan ganteng"
+    }
+  ]
+
+  const [activePopupKey, setActivePopupKey] = useState(null)
+  const [isEditMode, setIsEditMode] = useState(false)
+
   return (
     <Map
       initialViewState={{
@@ -11,6 +25,37 @@ function MapInteractive() {
       }}
       mapStyle="https://tiles.openfreemap.org/styles/bright"
     >
+      {markersData.map((marker) => (
+        <Marker
+          key={marker.key}
+          latitude={marker.latitude}
+          longitude={marker.longitude}
+          color="#000000"
+          onClick={(e) => {
+            e.originalEvent.stopPropagation();
+            if (!isEditMode) setActivePopupKey(marker.key);
+          }}
+        >
+        </Marker>
+      ))}
+
+      {markersData.map((marker) => {
+        if (activePopupKey !== marker.key) return null
+
+        return (
+          <Popup
+            latitude={marker.latitude}
+            longitude={marker.longitude}
+            anchor="bottom"
+            onClose={() => setActivePopupKey(null)}
+            closeOnClick={true}
+            closeButton={false}
+          >
+            <h3>{marker.title}</h3>
+            <p>{marker.description}</p>
+          </Popup>
+        )
+      })}
     </Map>
   )
 }
