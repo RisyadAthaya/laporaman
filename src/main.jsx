fbd9bcd8adc9fbeb39c4e87854fc479f1a579d2a
DioDateUs <deodatussihaloho@gmail.com>
2026-07-16 16:15:23 +0700

    add NavBar.jsx and LandingPage.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './pages/App.jsx'
+import LandingPage from './pages/LandingPage.jsx'
+import './styles/globals.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LandingPage />
   </StrictMode>,
 )
